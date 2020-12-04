be6aabc67e8d74767c0798510c377250f4fc4da0
Fajaru1503 <fajarudewangga@gmail.com>
2020-12-04 13:36:48 +0800

    add player attack method

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const app = Vue.createApp({
       monsterHealth: 100,
     };
   },
+  methods: {
+    playerAttack() {
+      const attackPower = Math.floor(Math.random() * (12 - 7)) + 7;
+      this.monsterHealth -= attackPower;
+      console.log(this.monsterHealth);
+    },
+  },
 });
 
 app.mount("#game");
